032de74127d5e3555efb409f3786de96d3bf93f0
johnjasperfernando <john.jasper2107@gmail.com>
2022-01-29 18:42:34 +0530

    first commit

---
+++ hibernatet-tutorial/src/com/lv2code/jdbc/TestJdbc.java
@@ -0,0 +1,25 @@
+package com.lv2code.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		
+		String jdbcUrl="jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String user="hbstudent";
+		String pass="hbstudent";
+		try {
+				System.out.println("connection to database:"+jdbcUrl);
+				Connection myConn=DriverManager.getConnection(jdbcUrl,user,pass);
+				System.out.println("connection Successfull");
+			}
+			catch(Exception exc)
+			{
+				exc.printStackTrace();
+			}
+	}
+
+}
